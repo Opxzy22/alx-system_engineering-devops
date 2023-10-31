e317fbc4171a46d81853cc728688ae0b11c63c96
opxzy22 <aopeyem84@gmail.com>
2023-10-31 12:57:56 -0700

    done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,43 +1,36 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API,
-for a given employee ID, returns information about
-TODO list progress.
+    Calls to data from the jsonplaceholder website parsed to json formats
+    from task 0, extend your Python script to export data in the JSON format.
 """
+if __name__ == "__main__":
+    """
+        Calls to data from the jsonplaceholder website
+        parsed to json formats from task 0, extend your
+        Python script to export data in the JSON format.
+    """
+    import json
+    import requests
+    import sys
 
-import json
-import requests
-import sys
+    EMPLOYEE_ID = sys.argv[1]
 
+    link = "https://jsonplaceholder.typicode.com/users"
+    userResponse = requests.get(f"{link}/{EMPLOYEE_ID}")
+    EMPLOYEE_NAME = userResponse.json().get("username")
+    fileName = "{}.json".format(EMPLOYEE_ID)
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} EMPLOYEE_ID")
-        sys.exit(1)
+    todoList = requests.get(f"{link}/{EMPLOYEE_ID}/todos").json()
 
-    employee_id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Error: Employee with ID {employee_id} not found.")
-        sys.exit(1)
+    tasks = []
+    for task in todoList:
+        taskDict = {}
+        taskDict["task"] = task.get('title')
+        taskDict["completed"] = task.get('completed')
+        taskDict["username"] = EMPLOYEE_NAME
+        tasks.append(taskDict)
 
-    employee_name = response.json().get("name")
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    response = requests.get(url)
-    todos = response.json()
-    total_tasks = len(todos)
-    completed_tasks = [todo for todo in todos if todo.get("completed")]
-    num_completed_tasks = len(completed_tasks)
+    todoDict = {EMPLOYEE_ID: tasks}
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employee_name, num_completed_tasks, total_tasks))
-    task_list = []
-    for task in todos:
-        task_dict = {"task": task.get("title"),
-                     "completed": task.get("completed"),
-                     "username": employee_name}
-        task_list.append(task_dict)
-    json_data = {employee_id: task_list}
-    with open(f"{employee_id}.json", "w") as outfile:
-        json.dump(json_data, outfile)
+    with open(fileName, "w", newline="") as fd:
+        json.dump(todoDict, fd)
