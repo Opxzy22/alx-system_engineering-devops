37d5a4693222b78fa701c65b94737ee59784bc5c
opxzy22 <aopeyem84@gmail.com>
2023-11-08 02:13:40 +0100

    workings

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.
+"""a function that queries the Reddit API and return
+s the number of subscribers (not active users, total subscribers) fo
+r a given subreddit. If an invalid subreddit is given
+, the function should return 0.
 """
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
